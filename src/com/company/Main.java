0ed7892de176fad0d2ae1a622700a3db68b2c1dc
Tienala <ronatavit@gmail.com>
2020-05-26 11:06:31 +0300

    яя

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte studingYear = 5;
+        System.out.println("I'm studing on the " + studingYear + "th course now");
+        short days = 8100;
+        System.out.println("Since my B-Day have passed more than "+ days+ " days");
+        int population = 41_980_000;
+        System.out.println("In my country live more than "+ population+ " people");
+        long mouseClicks = 5_252_365_142l;
+        System.out.println("I've clicked on mouse " + mouseClicks+ " times during my life");
+        char myMail = '@';
+        System.out.println( myMail+" exists in my E-mail adress");
+        float thikness = 0.000000000054f;
+        System.out.println("The thikness of my hair is "+ thikness +" m");
+        double weightCat = 4.45;
+        System.out.println("My cat weights " + weightCat + " kilos");
+
+
+        boolean isFemale = true;
+    }
+}
